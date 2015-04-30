fe92a35408d57c7c6faec82b3094dca2a0c03d26
Sujith Prabhu <s.prabhu@kainos.com>
2015-04-30 15:35:48 +0100

    Refactored to externalize constants and renamed tests

---
+++ src/main/java/com/tddshop/Constants.java
@@ -0,0 +1,7 @@
+package com.tddshop;
+
+public class Constants {
+	public static final String APPLE = "Apple";
+	public static final String ORANGE = "Orange";
+
+}
--- src/main/java/com/tddshop/ShoppingCart.java
+++ src/main/java/com/tddshop/ShoppingCart.java
@@ -2,16 +2,19 @@ package com.tddshop;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import static com.tddshop.Constants.APPLE;
+import static com.tddshop.Constants.ORANGE;
 
 public class ShoppingCart {
+	
 	private static Map<String, Double> prices;
+
 	static{
 		prices = new HashMap<String, Double>();
-		prices.put("Apple", 0.6);
-		prices.put("Orange", 0.25);
+		prices.put(APPLE, 0.6);
+		prices.put(ORANGE, 0.25);
 	}
 	List<String> cartItems = new ArrayList<String>();
 	
@@ -27,21 +30,21 @@ public class ShoppingCart {
 		double totalPrice = 0.0;
 		StringBuilder sb = new StringBuilder("[ ");
 		if(cartItems.size()>0){
-			Map<String, Integer> productQuantity = new HashMap();
+			Map<String, Integer> productQuantity = new HashMap<String, Integer>();
 			for (String item : cartItems) {
 				sb.append(item).append(" ");
 				int qty = productQuantity.containsKey(item) ? productQuantity.get(item): 0;
 				productQuantity.put(item, qty+1);
 			}
 			if(productQuantity.size()>0){
-				if(productQuantity.containsKey("Apple")){
-					int qty = productQuantity.get("Apple");
-					Double itemTotal =   (((int)qty /2) + (qty % 2)) * prices.get("Apple");
+				if(productQuantity.containsKey(APPLE)){
+					int qty = productQuantity.get(APPLE);
+					Double itemTotal =   (((int)qty /2) + (qty % 2)) * prices.get(APPLE);
 					totalPrice += itemTotal;
 				}
-				if(productQuantity.containsKey("Orange")){
-					int qty = productQuantity.get("Orange");
-					Double itemTotal =   ((((int)qty /3)*2) + (qty % 3)) * prices.get("Orange");
+				if(productQuantity.containsKey(ORANGE)){
+					int qty = productQuantity.get(ORANGE);
+					Double itemTotal =   ((((int)qty /3)*2) + (qty % 3)) * prices.get(ORANGE);
 					totalPrice += itemTotal;
 				}
 			}
@@ -54,8 +57,8 @@ public class ShoppingCart {
 	
 	public static void main(String a[]){
 		ShoppingCart cart = new ShoppingCart();
-		cart.addProduct("Apple");
-		cart.addProduct("Orange");
+		cart.addProduct(APPLE);
+		cart.addProduct(ORANGE);
 		cart.getTotalCost();
 	}
 
